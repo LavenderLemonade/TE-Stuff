4487a3b4a0d59c602667564dff3ad335aa998fc4
Sam Elliott <soe1@uakron.edu>
2022-09-16 20:33:40 -0400

    making sure changes are the same between my PC and laptop

--- csharp/module-1/05a_Command_Line_Programs/exercise/Fibonacci/Program.cs
+++ csharp/module-1/05a_Command_Line_Programs/exercise/Fibonacci/Program.cs
@@ -6,7 +6,21 @@ namespace Fibonacci
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int runningSum = 1;
+            int[] fibSeq = new int[34];
+            fibSeq[0] = 0;
+            fibSeq[1] = 1;
+            Console.WriteLine("Yo, welcome to the Fibonaci Sequence Generator!");
+            Console.WriteLine("Give me a number you'd like to see the Fibonacci Sequence of. Keep it to ints plz.");
+
+            for (int i = 2; runningSum <= 34; i++)
+            {
+                runningSum += fibSeq[i-1] + runningSum;
+                fibSeq[i] = runningSum;
+            }
+            string blue = string.Join("", fibSeq);
+            Console.WriteLine(blue);
+
         }
     }
 }
--- csharp/module-1/05a_Command_Line_Programs/lecture/DiscountCalculator/Program.cs
+++ csharp/module-1/05a_Command_Line_Programs/lecture/DiscountCalculator/Program.cs
@@ -10,16 +10,60 @@ namespace DiscountCalculator
         /// <param name="args"></param>
         static void Main(string[] args)
         {
+           
             Console.WriteLine("Welcome to the Discount Calculator");
+
 
             // Prompt the user for a discount amount
             // The answer needs to be saved as a double
-            Console.Write("Enter the discount amount (w/out percentage): ");
+            double thatDiscount = 0;
+
+            do {
+                Console.Write("Enter the discount amount (w/out percentage): ");
+
+                string discAmount = Console.ReadLine();
+                thatDiscount = double.Parse(discAmount);
+                thatDiscount = thatDiscount / 100.0;
+
+                if (thatDiscount > 100)
+                {
+                    Console.WriteLine("Come on dude, that's too much, relax.");
+                   
+
+                }
+
+                Console.WriteLine("You typed: " + thatDiscount);
+
+            }
+            while (thatDiscount > 100);
+
+
+
+
+
+            
 
 
 
             // Prompt the user for a series of prices
             Console.Write("Please provide a series of prices (space separated): ");
+            string morePrices = Console.ReadLine();
+
+            Console.WriteLine("You entered: " + morePrices);
+
+            string[] priceArray = morePrices.Split(" ");
+            for (int i =0; i < priceArray.Length; i++)
+            {
+                string value = priceArray[i];
+                decimal finPrice = decimal.Parse(value);
+                decimal finalPrice = finPrice - (finPrice * (decimal)thatDiscount);
+                Console.WriteLine($"Sale price: {finalPrice:C2}");
+
+            }
+            
+            
+
+
 
 
 
