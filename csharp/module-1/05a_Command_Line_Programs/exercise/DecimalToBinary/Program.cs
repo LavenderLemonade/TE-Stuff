461fe794837d5dccada5e4f0ec7c511d0b58aa6d
Sam Elliott <soe1@uakron.edu>
2022-09-19 08:06:15 -0400

    sending to myself

--- csharp/module-1/05a_Command_Line_Programs/exercise/DecimalToBinary/Program.cs
+++ csharp/module-1/05a_Command_Line_Programs/exercise/DecimalToBinary/Program.cs
@@ -6,7 +6,47 @@ namespace DecimalToBinary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Tossing my counter(s) here
+            int runningMod = 0;
+            int counter = 0;
+
+            //let's get this party started 
+            Console.WriteLine("Yo, gimme a number to convert");
+            string theQuery = Console.ReadLine();
+            double newNum = double.Parse(theQuery);
+            int[] keepTrack = new int[(int)(newNum)];
+
+            for (int i= 0; newNum >= 1; i++)
+            {
+                if ((newNum % 2) == 0 && i == counter)
+                {
+                    newNum = newNum / 2.0;
+                    runningMod = (int)(newNum % 2);
+                    counter++;
+                    keepTrack[i] = runningMod;
+                    
+                }
+
+                if ((newNum % 2) == 1 && i == counter)
+                {
+                    runningMod = (int)(newNum % 2);
+                    keepTrack[i] = runningMod;
+                    newNum = (newNum - 1) / 2.0;
+                    
+                    counter++;
+                    
+                    
+                }
+            }
+
+            int[] checkThat = new int[counter];
+            for (int j=0; j<counter; j++)
+            {
+                checkThat[j] = keepTrack[counter - (j + 1)];
+            }
+
+            string green = string.Join("", checkThat);
+            Console.WriteLine(green);
         }
     }
 }
--- csharp/module-1/05a_Command_Line_Programs/exercise/Fibonacci/Program.cs
+++ csharp/module-1/05a_Command_Line_Programs/exercise/Fibonacci/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks.Dataflow;
 
 namespace Fibonacci
 {
@@ -6,7 +9,55 @@ namespace Fibonacci
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Let's try the Fibonacci program!");
+            Console.WriteLine("Input a number, it needs to be an integer.");
+            string chosenNumber = Console.ReadLine();
+            int theOneNumber = int.Parse(chosenNumber);
+            int counter = 0;
+
+            int[] fibArray = new int[Math.Abs(theOneNumber) + 2];
+            fibArray[0] = 0;
+            fibArray[1] = 1;
+
+            if (theOneNumber <= 0)
+            {
+                int[] newArray = new int[2];
+                for (int y =0; y < 2; y++)
+                {
+                    newArray[y] = fibArray[y];
+                }
+                string lessZero = string.Join(",", newArray);
+                Console.WriteLine(lessZero);
+            }
+            if (theOneNumber == 1)
+            {  
+                fibArray[2] = 1;
+                string isOne = string.Join(",", fibArray);
+                Console.WriteLine(isOne);
+            }
+
+            for (int i =2; fibArray[i-1] + fibArray[i-2] <= theOneNumber; i++)
+            {
+                fibArray[i] = fibArray[i - 1] + fibArray[i - 2];
+            }
+
+            for (int j = 0; j < theOneNumber; j++)
+            {
+                if (fibArray[j] != 0 && fibArray[j] <= theOneNumber)
+                {
+                    counter++;
+                }
+            }
+
+            int[] finArray = new int[counter];
+
+            for (int z = 0; z < counter; z++)
+            {
+                finArray[z] = fibArray[z];
+            }
+
+            string green = string.Join(",", finArray);
+            Console.WriteLine(green);
         }
     }
 }
