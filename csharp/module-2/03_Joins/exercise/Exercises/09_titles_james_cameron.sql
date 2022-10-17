f0861b3ef23ca32f5ddb809d068074bbfd70a2bd
Sam Elliott <soe1@uakron.edu>
2022-10-16 21:58:20 -0400

    mmmm

--- csharp/module-2/03_Joins/exercise/Exercises/09_titles_james_cameron.sql
+++ csharp/module-2/03_Joins/exercise/Exercises/09_titles_james_cameron.sql
@@ -1,3 +1,10 @@
 -- 9. The titles of movies directed by James Cameron, sorted alphabetically.
 -- (6 rows)
 
+SELECT DISTINCT title FROM movie
+JOIN movie_actor ON movie_actor.movie_id = movie.movie_id
+JOIN person ON person.person_id = movie_actor.actor_id
+WHERE director_id = 2710
+
+
+
