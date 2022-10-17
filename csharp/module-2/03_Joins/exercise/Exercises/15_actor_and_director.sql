1582ac4d2ab992f0d0ca8d6d41057368bc9f04b9
Sam Elliott <soe1@uakron.edu>
2022-10-17 08:52:36 -0400

    AUGH

--- csharp/module-2/03_Joins/exercise/Exercises/15_actor_and_director.sql
+++ csharp/module-2/03_Joins/exercise/Exercises/15_actor_and_director.sql
@@ -2,8 +2,8 @@
 -- Order the results by movie title (A-Z)
 -- (73 rows)
 
-SELECT DISTINCT title, person_name FROM movie 
-JOIN person ON movie.director_id = person.person_id
-JOIN movie_actor ON person.person_id = movie_actor.actor_id
-WHERE actor_id = director_id 
+SELECT title, person_name FROM person
+JOIN movie_actor ON movie_actor.actor_id = person.person_id
+JOIN movie ON movie.movie_id = movie_actor.movie_id
+WHERE movie_actor.actor_id = movie.director_id 
 ORDER BY title
--- csharp/module-2/03_Joins/exercise/Exercises/16_name_born_1950s_acted_1985.sql
+++ csharp/module-2/03_Joins/exercise/Exercises/16_name_born_1950s_acted_1985.sql
@@ -5,5 +5,5 @@
 SELECT person_name, birthday FROM person
 JOIN movie_actor ON movie_actor.actor_id = person.person_id
 JOIN movie ON movie_actor.movie_id = movie.movie_id
-WHERE birthday BETWEEN '1950-01-01' AND '1959-12-31' AND release_date = 1985
-ORDER BY birthday DESC
+WHERE birthday BETWEEN '1950-01-01' AND '1959-12-31' AND release_date BETWEEN '1985-01-01'  AND '1985-12-31'
+ORDER BY birthday
