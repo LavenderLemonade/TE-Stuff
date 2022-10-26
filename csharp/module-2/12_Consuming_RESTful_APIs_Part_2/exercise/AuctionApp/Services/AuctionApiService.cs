7416df0732e077f36a538b9e6453d49ba729a262
Sam Elliott <soe1@uakron.edu>
2022-10-26 09:22:09 -0400

    lmao forgot

--- csharp/module-2/12_Consuming_RESTful_APIs_Part_2/exercise/AuctionApp/Services/AuctionApiService.cs
+++ csharp/module-2/12_Consuming_RESTful_APIs_Part_2/exercise/AuctionApp/Services/AuctionApiService.cs
@@ -76,17 +76,40 @@ namespace AuctionApp.Services
 
         public Auction AddAuction(Auction newAuction)
         {
-            throw new System.NotImplementedException();
+            RestRequest request = new RestRequest("auctions");
+            request.AddJsonBody(newAuction); //attach the data to the request 
+            IRestResponse<Auction> response = client.Post<Auction>(request); //send the request 
+            //check for success
+            CheckForError(response, $"Add reservation for {newAuction.Id}");
+            return response.Data;
         }
 
         public Auction UpdateAuction(Auction auctionToUpdate)
         {
-            throw new System.NotImplementedException();
+            RestRequest request = new RestRequest($"auctions/{auctionToUpdate.Id}");
+            request.AddJsonBody(auctionToUpdate); //add the object that is being updated 
+            IRestResponse<Auction> response = client.Put<Auction>(request); //keep in mind that PUT will overwrite the whole object on the server
+            CheckForError(response, $"Update auction for {auctionToUpdate.Id}");
+            return response.Data;
         }
 
         public bool DeleteAuction(int auctionId)
         {
-            throw new System.NotImplementedException();
+            RestRequest request = new RestRequest($"auctions/{auctionId}");
+            IRestResponse response = client.Delete(request); //no data type to deserialize into because delete doesn't return data 
+            CheckForError(response, $"Delete auction {auctionId}");
+            return true;
+        }
+
+        private void CheckForError(IRestResponse response, string action)
+        {
+            if (!response.IsSuccessful)
+            {
+                // TODO: Write a log message for future reference
+
+                throw new HttpRequestException($"There was an error in the call to the server");
+            }
+
         }
     }
 }
