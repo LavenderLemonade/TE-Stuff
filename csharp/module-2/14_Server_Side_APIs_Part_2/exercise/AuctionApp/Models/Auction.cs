83faa8849272457d8f8e8c8075324938655c7376
Sam Elliott <soe1@uakron.edu>
2022-10-31 08:44:19 -0400

    lmao

--- csharp/module-2/14_Server_Side_APIs_Part_2/exercise/AuctionApp/Models/Auction.cs
+++ csharp/module-2/14_Server_Side_APIs_Part_2/exercise/AuctionApp/Models/Auction.cs
@@ -6,12 +6,22 @@ namespace AuctionApp.Models
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "You need a title.")]
         public string Title { get; set; }
 
+        [Required(ErrorMessage = "You need a description.")]
         public string Description { get; set; }
 
+        [Required(ErrorMessage = "You need a username.")]
         public string User { get; set; }
 
+        [Range(1, 20, ErrorMessage = "Bid cannot be 0.")]
         public double CurrentBid { get; set; }
+        
+
+        public Auction()
+        {
+        }
+
     }
 }
