45ef0bcf3bdfc0416d6f7753569f6084c4233d07
Sam Elliott <soe1@uakron.edu>
2022-11-20 22:24:35 -0500

    pushing p again

--- csharp/module-3/08_DOM/exercise/shopping-list/js/shopping-list.js
+++ csharp/module-3/08_DOM/exercise/shopping-list/js/shopping-list.js
@@ -1,31 +1,88 @@
-// add pageTitle
-
-// add groceries
+let allItemsIncomplete = true;
+const pageTitle = 'My Shopping List';
+const groceries = [
+  { id: 1, name: 'Oatmeal', completed: false },
+  { id: 2, name: 'Milk', completed: false },
+  { id: 3, name: 'Banana', completed: false },
+  { id: 4, name: 'Strawberries', completed: false },
+  { id: 5, name: 'Lunch Meat', completed: false },
+  { id: 6, name: 'Bread', completed: false },
+  { id: 7, name: 'Grapes', completed: false },
+  { id: 8, name: 'Steak', completed: false },
+  { id: 9, name: 'Salad', completed: false },
+  { id: 10, name: 'Tea', completed: false }
+];
 
 /**
  * This function will get a reference to the title and set its text to the value
  * of the pageTitle variable that was set above.
  */
-function setPageTitle() {}
+function setPageTitle() {
+  const title = document.getElementById('title');
+  title.innerText = pageTitle;
+}
 
 /**
  * This function will loop over the array of groceries that was set above and add them to the DOM.
  */
-function displayGroceries() {}
+function displayGroceries() {
+  const ul = document.querySelector('ul');
+  groceries.forEach((item) => {
+    const li = document.createElement('li');
+    li.innerText = item.name;
+    const checkCircle = document.createElement('i');
+    checkCircle.setAttribute('class', 'far fa-check-circle');
+    li.appendChild(checkCircle);
+    ul.appendChild(li);
+  });
+}
 
-/**
- * This function will be called when the button is clicked. You will need to get a reference
- * to every list item and add the class completed to each one
- */
-function markCompleted() {}
+window.addEventListener('DOMContentLoaded', (event) => {
+  setPageTitle();
+  displayGroceries();
 
-setPageTitle();
+  completeButtons = document.querySelectorAll("li");
+  completeButtons.forEach((item) => {
+    item.addEventListener('click', event => {
+      item.setAttribute('class','completed');
+      item.lastChild.setAttribute('class','completed');
+    })
+  });
 
-displayGroceries();
+  completeButtons.forEach((item) => {
+    item.addEventListener('dblclick', event => {
+      if (item.className == 'completed')
+      {
+        item.removeAttribute('class');
+        item.lastChild.removeAttribute('class');
+      }
+    })
+  });
 
-// Don't worry too much about what is going on here, we will cover this when we discuss events.
-document.addEventListener('DOMContentLoaded', () => {
-  // When the DOM Content has loaded attach a click listener to the button
-  const button = document.querySelector('.btn');
-  button.addEventListener('click', markCompleted);
+  markAll =  document.getElementById('toggleAll');
+  
+  markAll.addEventListener('click', event => {
+    completeButtons.forEach((item => {
+      item.setAttribute('class','completed');
+      item.lastChild.setAttribute('class','completed');
+    }))
+    markAll.innerText = "Mark All Incomplete"
+  });
+
+  markAll.addEventListener('dblclick', event => {
+    completeButtons.forEach((item => {
+      item.setAttribute('class','incomplete');
+      item.lastChild.setAttribute('class','incomplete');
+    }))
+    markAll.innerText = "Mark All Complete"
+  });
+  
+
+  
+
+
+
 });
+
+
+
