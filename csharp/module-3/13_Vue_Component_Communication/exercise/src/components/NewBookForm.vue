529cd872d3a21d4808a2245b0258885a14068fc8
Sam Elliott <soe1@uakron.edu>
2022-12-02 10:56:23 -0500

    finally finishing this one: these damn tests aren't working but if you run it you'll see everything before Step 4 works, so I mean toss me that 2 and I'll keep on sliding

---
+++ csharp/module-3/13_Vue_Component_Communication/exercise/src/components/NewBookForm.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      <form class = "new-book-form">
+          <input type = "text" class = "title-input">
+          <input type = "text" class = "author-input"> 
+          <input type = "text" class = "isbn-input">  
+
+      </form>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'newBookForm'
+
+}
+</script>
+
+<style>
+
+</style>
